6334b4b89505ad7a97bdbf2405ae10d53f390a66
sravya-kanagarla <sravya.kanagarla96@gmail.com>
2015-11-21 21:26:01 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,47 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
+void reverse(char *str, int len) {
+	int i;
+	char t;
+	for (i = 0; i < len; i++) {
+		t = str[i];
+		str[i] = str[len];
+		str[len] = t;
+		len--;
+	}
+}
 
+int copyInt(int decimalPart, char *str) {
+	int i;
+	for (i = 0; decimalPart; decimalPart /= 10) {
+		str[i++] = (decimalPart % 10) + '0';
+	}
+	str[i] = '\0';
+	reverse(str, i - 1);
+	return i;
+}
 
-void number_to_str(float number, char *str,int afterdecimal){
-	
+void number_to_str(float number, char *str, int afterdecimal){
+	int index = 0;
+	int decimalPart;
+	if (number < 0) {
+		str[index++] = '-';
+		number *= -1;
+	}
+	decimalPart = (int)number;
+	index = copyInt(decimalPart, &str[index]);
+	if (str[0] == '-')
+		index++;
+	number -= (float)decimalPart;
+	decimalPart = 1;
+	if (afterdecimal) {
+		str[index++] = '.';
+		while (afterdecimal) {
+			decimalPart *= 10;
+			afterdecimal--;
+		}
+		number *= decimalPart;
+		copyInt((int)number, &str[index]);
+	}
 }
